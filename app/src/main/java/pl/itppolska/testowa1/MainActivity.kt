a0bb3dd921909ce7385d01d1402739b0bda2216d
tdzierzega <tdzierzega@gmail.com>
2019-08-21 18:22:06 +0200

    Wgranie po zakonczonym odcinku 2.5

--- app/src/main/java/pl/itppolska/testowa1/MainActivity.kt
+++ app/src/main/java/pl/itppolska/testowa1/MainActivity.kt
@@ -21,9 +21,11 @@ class MainActivity : AppCompatActivity() {
 
         btnSendMessage.setOnClickListener {
             val message : String = edUserMessage.text.toString()
-            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+           // Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 
             val intent = Intent(this, SecondActivity::class.java)
+
+            intent.putExtra("user_message",message)
             startActivity(intent)
         }
 
