bbdd0e2ade3fe2cd7954a2e186c5f273e692fb0e
tdzierzega <tdzierzega@gmail.com>
2019-08-21 16:24:43 +0200

    Pierwsze wgranie

--- app/src/main/java/pl/itppolska/testowa1/MainActivity.kt
+++ app/src/main/java/pl/itppolska/testowa1/MainActivity.kt
@@ -2,13 +2,22 @@ package pl.itppolska.testowa1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnShowToast.setOnClickListener {
+            //Code
+            Log.i("MainActivity", "Button nacisniety")
+
+            Toast.makeText(this, "Nacisniety button 1", Toast.LENGTH_SHORT).show()
+
+        }
 
     }
-
 }
