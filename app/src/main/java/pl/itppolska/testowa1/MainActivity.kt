cc68b29aa48b096e92971630a0043eaf4dd5b568
tdzierzega <tdzierzega@gmail.com>
2019-08-21 14:42:26 +0200

    Pierwsze wgranie

---
+++ app/src/main/java/pl/itppolska/testowa1/MainActivity.kt
@@ -0,0 +1,13 @@
+package pl.itppolska.testowa1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
