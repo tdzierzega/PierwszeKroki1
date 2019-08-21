9de7dd263d4b2e953f8518304f8979a6504ca5eb
tdzierzega <tdzierzega@gmail.com>
2019-08-21 17:45:56 +0200

    Wgranie po zakonczonym odcinku

--- app/src/main/java/pl/itppolska/testowa1/MainActivity.kt
+++ app/src/main/java/pl/itppolska/testowa1/MainActivity.kt
@@ -1,5 +1,6 @@
 package pl.itppolska.testowa1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -11,12 +12,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         btnShowToast.setOnClickListener {
             //Code
             Log.i("MainActivity", "Button nacisniety")
-
             Toast.makeText(this, "Nacisniety button 1", Toast.LENGTH_SHORT).show()
+        }
 
+        btnSendMessage.setOnClickListener {
+            val message : String = edUserMessage.text.toString()
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
         }
 
     }
